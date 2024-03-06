951695e88d310a3898d2e8d7a944eba79b85716d
carlos676767 <carlitobr730@gmail.com>
2024-03-05 18:29:38 -0800

    agora da para remover itens

--- grud/src/main.ts
+++ grud/src/main.ts
@@ -1,17 +1,14 @@
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
-const nomeProdutoDoProduto = document.getElementById("productName") as HTMLInputElement
-const precoProduto = document.getElementById("productPrice") as HTMLInputElement
-const quantidadeProduto = document.getElementById("productQuantity") as HTMLInputElement
+const nomeProdutoDoProduto = document.getElementById("productName") as HTMLInputElement;
+const precoProduto = document.getElementById("productPrice") as HTMLInputElement;
+const quantidadeProduto = document.getElementById("productQuantity") as HTMLInputElement;
 
 interface Mercadorias {
   nomeProduto: string;
   preco: string;
   quantidade: string;
 }
-
-
-
 
 const cadastrarProdutos = () => {
   const mercadorias: Mercadorias = {
@@ -45,27 +42,67 @@ const mensagemVazio = () => {
   });
 };
 
-
-const botaoPesquisar = document.getElementById("botao-pesquisar")as HTMLButtonElement;
-
+const botaoPesquisar = document.getElementById("botao-pesquisar") as HTMLButtonElement;
+const deltarProduto = document.getElementById("deltar-produto") as HTMLButtonElement;
+const buscarProdutoInput = document.getElementById("searchInput") as HTMLInputElement;
+let produtosSelecionados: any
+const productList = document.querySelector("ul") as HTMLUListElement; 
 const buscarProdutoPorNome = () => {
-  const productList = document.querySelector("ul") as HTMLUListElement; //exibe os produtos
-  const buscarProdutoInput = document.getElementById("searchInput") as HTMLInputElement;
   if (buscarProdutoInput.value === "") {
-    mensagemVazio()
+    mensagemVazio();
   } else {
     fetch(`http://localhost:3000/produtos?nomeProduto=${buscarProdutoInput.value}`)
       .then((response) => response.json())
-      .then((data) => {
+      .then((data: Array<any>) => {
+        let produtos: any = []
         console.log(data[0].nomeProduto);
-        productList.innerHTML = `
-        <div><i class="fa-solid fa-cube"></i>${JSON.stringify( data[0].nomeProduto ).replace(/"/g, "")}</div> <div><i class="fa-solid fa-sack-dollar"></i>${JSON.stringify( data[0].preco  ).replace(/"/g, "")}</div> <div><i class="fa-solid fa-chart-column"></i>${JSON.stringify(data[0].quantidade ).replace(/"/g, "")}</div> `;
+        data.forEach(element => {
+        let produto = `
+        <div>
+             <i class="fa-solid fa-cube"></i>${JSON.stringify(element.nomeProduto).replace(/"/g, "")}
+           </div>
+           <div>
+               <i class="fa-solid fa-sack-dollar"></i>${JSON.stringify(element.preco).replace(/"/g, "")}
+           </div>
+           <div>
+               <i class="fa-solid fa-chart-column"></i>${JSON.stringify(element.quantidade).replace(/"/g, "")}
+           </div>
+           <div>
+           <button id="deltar-produto">Deletar</button>
+       </div>
+        `
+        produtos.push(produto)
+        })
+        productList.innerHTML = produtos
+        produtosSelecionados = data[0]
+        deltarProduto.style.display = "block";
       })
       .catch((error) => {
         (productList.textContent = "Produto não encontrado"), error;
       });
   }
 };
+
+const deletarProduto = () => {
+  fetch(
+    `http://localhost:3000/produtos/${produtosSelecionados.id}`,
+    { method: "DELETE", })
+    .then((response) => {
+      if (response.ok) {
+        console.log("Item excluído com sucesso");
+        productList.textContent = ""
+      } else {
+        console.error("Erro ao excluir o item");
+      }
+    })
+    .catch((error) => {
+      console.error("Erro na solicitação DELETE:", error);
+    });
+};
+
+deltarProduto.addEventListener("click", () => {
+  deletarProduto();
+});
 
 botaoPesquisar.addEventListener("click", () => {
   buscarProdutoPorNome();
