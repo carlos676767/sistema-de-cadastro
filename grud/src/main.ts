c699ded7b98754e77ab99943ec5b1f343817d224
carlos676767 <carlitobr730@gmail.com>
2024-03-08 17:27:59 -0800

    adcionado buscar imagens

--- grud/src/main.ts
+++ grud/src/main.ts
@@ -1,8 +1,8 @@
 import Swal from "sweetalert2";
 
-const nomeProdutoDoProduto = document.getElementById("productName") as HTMLInputElement;
+const nomeProdutoDoProduto = document.getElementById( "productName") as HTMLInputElement;
 const precoProduto = document.getElementById("productPrice") as HTMLInputElement;
-const quantidadeProduto = document.getElementById("productQuantity") as HTMLInputElement;
+const quantidadeProduto = document.getElementById( "productQuantity") as HTMLInputElement;
 
 interface Mercadorias {
   nomeProduto: string;
@@ -11,9 +11,9 @@ interface Mercadorias {
 }
 
 const cadastrarProdutos = () => {
-  if (nomeProdutoDoProduto.value === "" && precoProduto.value === "" && quantidadeProduto.value === "") {
-    mensagemVazio()
-  }else{
+  if ( nomeProdutoDoProduto.value === "" &&precoProduto.value === "" && quantidadeProduto.value === "") {
+    mensagemVazio();
+  } else {
     const mercadorias: Mercadorias = {
       nomeProduto: nomeProdutoDoProduto.value,
       preco: precoProduto.value,
@@ -22,6 +22,20 @@ const cadastrarProdutos = () => {
     requsicaoPostAdicionarProdutos(mercadorias);
   }
 };
+
+
+  nomeProdutoDoProduto.addEventListener("input", () => {
+    const imagemProduto = document.getElementById("imagemProduto") as HTMLImageElement
+    fetch(`https://api.unsplash.com/photos/random?query=${nomeProdutoDoProduto.value}&client_id=47jhOV5e1cn6V3CwVPBw03RP7luThokL663nIOpWheo`)
+    .then(response => response.json())
+    .then(data => {
+      imagemProduto.src = data.urls.regular
+    })
+    .catch(erro => {
+      console.error(erro)
+    })
+  })
+
 
 const requsicaoPostAdicionarProdutos = (objeto: {}) => {
   fetch("http://localhost:3000/produtos", {
@@ -34,7 +48,7 @@ const requsicaoPostAdicionarProdutos = (objeto: {}) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      mensagemProdutoCadastrado()
+      mensagemProdutoCadastrado();
     });
 };
 
@@ -51,32 +65,31 @@ const mensagemValorNaoEncontrado = () => {
   Swal.fire({
     title: "Oops...",
     text: "Produto não encontrado",
-    icon: "error"
+    icon: "error",
   });
-}
+};
 
 const mensagemProdutoDeletado = () => {
   Swal.fire({
     title: "Sucesso!",
     text: "Produto deletado com sucesso",
-    icon: "success"
+    icon: "success",
   });
-}
+};
 
 const mensagemProdutoCadastrado = () => {
   Swal.fire({
     title: "Sucesso!",
     text: "Produto cadastrado com sucesso",
-    icon: "info" 
+    icon: "info",
   });
-}
+};
 
-
-const botaoPesquisar = document.getElementById("botao-pesquisar") as HTMLButtonElement;
-const deltarProduto = document.getElementById("deltar-produto") as HTMLButtonElement;
+const botaoPesquisar = document.getElementById( "botao-pesquisar") as HTMLButtonElement;
+const deltarProduto = document.getElementById( "deltar-produto") as HTMLButtonElement;
 const buscarProdutoInput = document.getElementById("searchInput") as HTMLInputElement;
-let produtosSelecionados: any
-const tabela = document.querySelectorAll("td") 
+let produtosSelecionados: any;
+const tabela = document.querySelectorAll("td");
 
 const buscarProdutoPorNome = () => {
   if (buscarProdutoInput.value === "") {
@@ -85,36 +98,44 @@ const buscarProdutoPorNome = () => {
     fetch(`http://localhost:3000/produtos?nomeProduto=${buscarProdutoInput.value}`)
       .then((response) => response.json())
       .then((data: Array<any>) => {
-        let produtos: any = []
+        let produtos: any = [];
         console.log(data[0].nomeProduto);
-        data.forEach(element => {
-        let produto = `
-        ${tabela[0].innerHTML =  JSON.stringify(element.nomeProduto).replace(/"/g, "")}
-        ${tabela[1].innerHTML =  JSON.stringify(element.preco).replace(/"/g, "")}
-        ${tabela[2].innerHTML =  JSON.stringify(element.quantidade).replace(/"/g, "")}
-        `
-        produtos.push(produto)
-        })
-        produtosSelecionados = data[0]
+        data.forEach((element) => {
+          let produto = `
+        ${(tabela[0].innerHTML = JSON.stringify(element.nomeProduto).replace(
+          /"/g,
+          ""
+        ))}
+        ${(tabela[1].innerHTML = JSON.stringify(element.preco).replace(
+          /"/g,
+          ""
+        ))}
+        ${(tabela[2].innerHTML = JSON.stringify(element.quantidade).replace(
+          /"/g,
+          ""
+        ))}
+        `;
+          produtos.push(produto);
+        });
+        produtosSelecionados = data[0];
       })
       .catch((error) => {
         console.log(error);
-        mensagemValorNaoEncontrado()
-
+        mensagemValorNaoEncontrado();
       });
   }
 };
 
 const deletarProduto = () => {
-  fetch(
-    `http://localhost:3000/produtos/${produtosSelecionados.id}`,
-    { method: "DELETE" })
+  fetch(`http://localhost:3000/produtos/${produtosSelecionados.id}`, {
+    method: "DELETE",
+  })
     .then((response) => {
       if (response.ok) {
-        mensagemProdutoDeletado()
+        mensagemProdutoDeletado();
         console.log("Item excluído com sucesso");
-        tabela.forEach(element => {
-          element.textContent = ''
+        tabela.forEach((element) => {
+          element.textContent = "";
         });
       } else {
         console.error("Erro ao excluir o item");
@@ -132,6 +153,9 @@ deltarProduto.addEventListener("click", () => {
 botaoPesquisar.addEventListener("click", () => {
   buscarProdutoPorNome();
 });
+
+
+
 
 
 const botaoCadastrar = document
